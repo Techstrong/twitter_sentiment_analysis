9c9c8c7f84973689d68723f1f754a17f6c02611c
Techstrong <47997587+Techstrong@users.noreply.github.com>
2019-03-05 22:40:50 -0600

    added Twitter API credentials

---
+++ twi_api_credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'rsQxZFh2FGfDFW9Gm5Ru9VU7r'
+consumer_secret = 'LLPcjtvavqeYATl6qyz1yO3bl9jzTib7tS1dsM8mst0lEYLUlt'
+access_token = '1072982501058957312-koZ2vdq3qQT94AwYl5Xt3k7pOcXvIT'
+access_secret = 'X7zuI1BWxS03dM42iXj0s3i5VUWd1GDLZiYpsAdNrS7zd'
